5833e0b7ac6f6af1986f031ccb3de6bcb606edb6
Alvaro Noguer <alvaronoguer@gmail.com>
2022-08-13 15:45:17 +0200

    Lab done

---
+++ my-first-project/src/components/ToDoItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+     <h1>{{ msg2 }}</h1>
+     <button @click="helloWorld"  >Click me!</button>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    name: 'ToDoItem',
+    props: {
+    msg2: String,
+  },
+    methods: {
+        helloWorld(){
+            console.log("hello world")
+        }
+    }
+};
+
+
+</script>
+
+<style>
+
+</style>
---
+++ my-first-project/src/views/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+ <div class="helloworld">
+
+ </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
